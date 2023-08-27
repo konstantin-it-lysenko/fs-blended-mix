402c2e9a529b2dc2f2cb71e70bed6f499216c239
Denys-Nalyvaiko <nalyvaikodenys@gmail.com>
2023-08-27 19:29:30 +0200

    task-5

--- lesson_3/modul_5.js
+++ lesson_3/modul_5.js
@@ -62,3 +62,29 @@
 // const logFi = user.loginFail.bind(user);
 // // askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
 // askPassword(logOk, logFi);
+
+//TODO: № 5 на class-и  =============================================
+//Напиши функцію конструктор User для створення користувача з такими властивостями
+//a. userName - ім'я, рядок
+//b. age - вік, число
+//c. numbersOfPost - кількість постів, число
+//d. клас очікує 1 параметр - об'єкт налаштувань з однойменними властивостями
+
+//Додай метод getInfo(), який повертає рядок:
+//`Користувачеві ${} ${} років і в нього ${} публікацій.`
+
+class User {
+  constructor({ userName, age, numbersOfPost } = {}) {
+    this.userName = userName;
+    this.age = age;
+    this.numbersOfPost = numbersOfPost;
+  }
+
+  getInfo() {
+    return `Користувачеві ${this.userName} ${this.age} років і в нього ${this.numbersOfPost} публікацій.`;
+  }
+}
+
+const mango = new User({ age: 20, numbersOfPost: 10, userName: "mango" });
+console.log(mango.getInfo());
+console.log(typeof User);
