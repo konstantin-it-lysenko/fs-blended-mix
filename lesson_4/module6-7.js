087e980dcd79784fd3b057b8709d0dc4252e167d
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-09-10 19:43:00 +0300

    complete task 2

--- lesson_4/module6-7.js
+++ lesson_4/module6-7.js
@@ -1,1 +1,17 @@
+//TODO: № 2 ==============================================
+// Після натискання кнопки "SWAP ME" здійснюється обмін вмістом між двома інпутами.
+// Ви можете натиснути на неї кілька разів або вручну змінити вміст інпутів.
 
+// const swapBtn = document.getElementById('swapButton');
+// const firstInputEl = document.getElementById('leftSwapInput');
+// const secondInputEl = document.getElementById('rightSwapInput');
+
+// swapBtn.addEventListener('click', targetBtnHandler);
+
+// function targetBtnHandler() {
+//     const firstValue = firstInputEl.value;
+//     const secondValue = secondInputEl.value;
+
+//     firstInputEl.value = secondValue;
+//     secondInputEl.value = firstValue;
+// }
